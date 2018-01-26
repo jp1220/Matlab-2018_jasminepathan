8368e96912fa34427875cf21a42cb7607970f892
Jasmine Pathan <jp1220@uw.edu>
2018-01-26 00:15:20 -0800

    push remove chp3 hw

--- Homework_Chapter4_JasminePathan.m
+++ Homework_Chapter4_JasminePathan.m
@@ -27,8 +27,8 @@ mat1
 %     25    25    25    25    25];
 
 mat2=ones(5);
-if i=1:5
-    mat2(i,:)=i*5;
+for i=1:5
+        mat2(i,:)=5*i;
 end
 mat2
 
@@ -38,41 +38,77 @@ mat2
 %     19    18    17    16    15; ...
 %     24    23    22    21    20];
 
-mat3=zeros(5);
-if i=1:5
+mat3=ones(5);
+    for j=1:5
+        mat3(:,j)=((j-1)*5)+[1:5]-1;
+    end
+mat3=(flip(mat3))';
+mat3
     
 % mat4=[1     2     3     4     5; ...
 %      6     7     8     9    10; ...
 %     11    12    13    14    15; ...
 %     16    17    18    19    20];
 
+mat4=ones(4,5);
+for i=1:4
+    mat4(i,:)=(i-1)*5+[1:5];
+end
+mat4
+
 % mat5=[19    18    17    16    15; ...
 %     14    13    12    11    10; ...
 %      9     8     7     6     5; ...
 %      4     3     2     1     0]; ...
 
+mat5=zeros(4,5);
+for i=1:4
+        mat5(i,:)=(i*j)-[1:5];
+end
+mat5=flip(mat5);
+mat5
+    
 % mat6=[0     0     0     0     0; ...
 %     10    10    10    10    10; ...
 %     20    20    20    20    20; ...
 %     30    30    30    30    30];
+
+mat6=zeros(4,5);
+for i=1:4
+    mat6(i,:)=(i-1)*10;
+end
+mat6
 
 % mat7=[0     1     0     1     0; ...
 %      1     0     1     0     1; ...
 %      0     1     0     1     0; ...
 %      1     0     1     0     1];
 
+mat7=zeros(4,5);
+for i=1:4
+    for j=1:5
+        if mod(i+j,2)==1
+            mat7(i,j)=1;
+        end
+    end
+end
+mat7
+        
+
 %Q 4.2: Indexing, matrices, length and for
 %You run an experiment and the data is collected into the following 3D matrix
 
-% mat(:,:,1) = [9    16    16    18     4; ...
-%      6    16    11    11     6; ...
-%     10    13     7    12     9; ...
-%     10     8    19    12     5];
+mat=zeros(4,5,2);
 
-% mat(:,:,2) = [17     5     9     9    12; ...
-%      4     9     4     2     5; ...
-%      5     6    18     5    12; ...
-%      3    18    20     8    14];
+mat(:,:,1) = [9    16    16    18     4;
+     6    16    11    11     6;
+    10    13     7    12     9;
+    10     8    19    12     5];
+
+mat(:,:,2) = [17     5     9     9    12;
+     4     9     4     2     5;
+     5     6    18     5    12;
+     3    18    20     8    14];
 
 % The rows represent repeated measurements, 
 % the columns represent subjects, 
@@ -83,12 +119,42 @@ if i=1:5
 
 % a) Find out how many scores there are greater or 
 % equal to 15.
-
+scores=0;
+for i=1:4
+    for j=1:5
+        for k=1:2
+            if mat(i,j,k)>=15
+                scores=scores+1;
+            end
+        end
+    end
+end
+scores
+                
 % b) Find out how many scores greater or equal to 15 
 % there were among people who didn’t get the cup of tea.
+    %assuming mat(:,:,1) was the cup of tea treatment
+ScoresNoTea=0;
+for i=1:4
+    for j=1:5
+        if mat(i,j,2)>=15
+            ScoresNoTea=ScoresNoTea+1;
+        end
+    end
+end
+ScoresNoTea
 
 %c) Set all the values less than or equal to 4 to NaN.
-
+for i=1:4
+    for j=1:5
+        for k=1:2
+            if mat(i,j,k)<=4
+                mat(i,j,k)=NaN;
+            end
+        end
+    end
+end
+mat
 % d) Calculate the mean for each subject 
 % (not including values less than or equal to 4).
 % If you feel very brave do it without using nonanmean from 
@@ -121,6 +187,36 @@ if i=1:5
 % displays the sum of the four dice. 
 % The program should count the number of rolls until
 % the sum of the four dice is equal to 20.
+%% and for the brave among you (xtra credit)
+
+clear all
+
+nsub=20;
+% generate a random group of men and women
+men=69+randn(nsub, 1)*3;
+women=66+randn(nsub, 1)*3;
+% calculate the real difference between them
+realdiff=mean(men)-mean(women);
+% throw all heights into a cauldron.
+
+all=[men ;women];
+
+% calculated the expected distribution of differences in means between men and women with a sample of 20 
+% if there was no genuine height difference
+for r=1:1000
+    % stir the cauldron
+    all=shuffle(all);
+    % assume first 20 are men, second 20 are women
+    m_men(r)=mean(all(1:nsub));
+    m_women(r)=mean(all(nsub+1:end));
+    m_diff(r)=m_men(r)-m_women(r);
+end
+
+% Modify the code to find out whether the height difference between men and women would be significant 
+% with 10 individuals, 20, 100. (in matlab the command is ttest).
+
+% in a group of 100 individuals, of whom 50 were male, 50 female. 
+% How many of the 50 tallest people would be female?
 
 
  	 
